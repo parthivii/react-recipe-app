d7f306ed36d00d88907914128fa80b250a97a263
parthivii <parthivisrivastava14@gmail.com>
2020-09-29 14:41:13 +0530

    Updates

---
+++ precache-manifest.df7deacdd5bf0d8673bdbc44ed0c7a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b48e30bb63459046c7ed7924c1f383",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "75cf797ee6b07008195d",
+    "url": "/react-recipe-app/static/css/main.4a7dda49.chunk.css"
+  },
+  {
+    "revision": "94dea1339d284a0783fe",
+    "url": "/react-recipe-app/static/js/2.b54d3e8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe-app/static/js/2.b54d3e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cf797ee6b07008195d",
+    "url": "/react-recipe-app/static/js/main.ab2160a2.chunk.js"
+  },
+  {
+    "revision": "209bb40ddcfb53baae52",
+    "url": "/react-recipe-app/static/js/runtime-main.84029532.js"
+  }
+]);
